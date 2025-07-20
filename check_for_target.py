7a8f076a8e8448efe803ea3f30f28b112880072c
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2025-07-20 16:59:20 +0530

    Update and rename Check For target to check_for_target.py

--- check_for_target.py
+++ check_for_target.py
@@ -1,8 +1,8 @@
-Question: Given a sorted array of unique integers and a target integer, return true if there exists a pair of numbers that sum to target, false otherwise. This problem is similar to Two Sum. (In Two Sum, the input is not sorted).
+#Question: Given a sorted array of unique integers and a target integer, return true if there exists a pair of numbers that sum to target, false otherwise. This problem is similar to Two Sum. (In Two Sum, the input is not sorted).
 
-For example, given nums = [1, 2, 4, 6, 8, 9, 14, 15] and target = 13, return true because 4 + 9 = 13.
+#For example, given nums = [1, 2, 4, 6, 8, 9, 14, 15] and target = 13, return true because 4 + 9 = 13.
 
-Solution:
+#Solution:
 def check_for_target(nums,target):
     left=0
     right=len(nums)-1
