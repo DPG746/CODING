08117358c21a7d848baaf562dba50b4a4c1285c1
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2025-08-22 20:27:27 +0530

    Time: 38 ms (40.43%), Space: 18.4 MB (24.82%) - LeetHub

---
+++ 1739-split-two-strings-to-make-palindrome/1739-split-two-strings-to-make-palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkPalindromeFormation(self, a: str, b: str) -> bool:
+        def is_palindrome(s, l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        
+        def check(x, y):
+            l, r = 0, len(x) - 1
+            while l < r and x[l] == y[r]:
+                l += 1
+                r -= 1
+            # If mismatch happens, check if remaining is palindrome in either string
+            return is_palindrome(x, l, r) or is_palindrome(y, l, r)
+
+        return check(a, b) or check(b, a)
